5e51a758d156de968fd1f3f5bf4a539c92b73e17
Anant Shankar <ashank@microsoft.com>
2018-09-20 15:23:38 +0530

    Packaging

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='SFMergeUtility',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='SFMergeUtility',
+    long_description=open('README.txt').read(),
+    install_requires=[''],
+    url='',
+    author='SFDev',
+    author_email='SFDev'
+)
